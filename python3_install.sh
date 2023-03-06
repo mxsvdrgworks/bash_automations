664bf74207e9d17df49ed620ee7a026e3347dbf8
mxsvdrgworks <84321862+mxsvdrgworks@users.noreply.github.com>
2023-03-06 10:27:17 +0100

    Update python3_install.sh

--- python3_install.sh
+++ python3_install.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+#Installing python3, run this script under the sudo user
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install python3.11
+
+#Checking version
+python --version

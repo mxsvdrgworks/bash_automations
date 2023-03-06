8920bb5df14d72ee4cf2abe21a8c3c8c738d1df1
mxsvdrgworks <84321862+mxsvdrgworks@users.noreply.github.com>
2023-03-06 11:26:45 +0100

    Update mysql_db_deploy.sh

--- mysql_db_deploy.sh
+++ mysql_db_deploy.sh
@@ -3,6 +3,7 @@
 #Variables
 date=$(date '+%Y-%m-%d %H:%M:%S')
 db_archive_folder=~/Downloads
+db_archive_file=wp_db.tar.gz
 db_backup_file=wp_db.sql
 db_user=wp_user
 db_host=127.0.0.1
@@ -12,7 +13,7 @@ db=wp_database
 cd $db_archive_folder
 echo "Extracting database archive"
 sleep 5
-tar -xf $db_archive.tar.gz
+tar -xf $db_archive_file
 echo "Extraction finished, check the folder $db_archive_folder"
 sleep 5
 echo "Deploying database dump into $db database"

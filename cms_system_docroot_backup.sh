0ca14e2f6165d93b38edc7b34c1220915c462786
mxsvdrgworks <84321862+mxsvdrgworks@users.noreply.github.com>
2023-03-07 15:05:09 +0100

    Update cms_system_docroot_backup.sh

--- cms_system_docroot_backup.sh
+++ cms_system_docroot_backup.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+#Variables
+cms_system=wait user input
+cms_docroot=
+#Script logic
+if [ $cms_system = 1 ]; then
+	echo "Wordpress docroot in the $cms_docroot"
+elif [ $cms_system = 2 ]; then
+	echo "Drupal docroot in the $cms_docroot"
+elif [ $cms_system = 3 ]; then
+	echo "Joomla docroot in the $cms_docroot"
+elif [ $cms_system = 4 ]; then
+	echo "Opencart docroot in the $cms_docroot"
+else
+	echo "You are an elder."
+fi
